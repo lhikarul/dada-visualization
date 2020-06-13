6cc928f76b5fb0e737eb0496ee95153e043ecd0f
lhiakrul <711hikaru@gmail.com>
2020-06-13 15:31:49 +0800

    add: import v-charts package

--- data-report-dev/src/main.js
+++ data-report-dev/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './plugins/vcharts'
 import Echarts from 'echarts'
 import VueEcharts from 'vue-echarts'
 

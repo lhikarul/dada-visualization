1c2ec7998f436ada603e11fa4c38eb73b168a55e
lhiakrul <711hikaru@gmail.com>
2020-06-13 15:19:57 +0800

    add: import vue-echarts package

--- data-report-dev/src/components/SalesView/index.vue
+++ data-report-dev/src/components/SalesView/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-      sales
-  </div>
+    <v-chart
+      :options="data"
+    />
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      data: {
+        xAxis: {
+          type: 'category'
+        },
+        yAxis: {},
+        series: [{
+          type: 'line',
+          data: [100, 200, 300]
+        }]
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.echarts {
+  width: 100%;
+  height: 100%;
+}
 </style>

d3aca3d7caaa2ac55a7948c511702916d5f5a14d
lhiakrul <711hikaru@gmail.com>
2020-06-13 14:14:41 +0800

    add: 累計訂單量組件開發

--- data-report-dev/src/components/TotalOrders/index.vue
+++ data-report-dev/src/components/TotalOrders/index.vue
@@ -1,15 +1,56 @@
 <template>
-  <common-card />
+  <common-card
+    title="累計訂單量"
+    value="$ 2,039,155"
+  >
+    <template>
+      <div id="total-orders-chart" :style="{width:'100%',height:'100%'}"></div>
+    </template>
+    <template v-slot:footer>
+      <span>昨日訂單量</span>
+      <span class="emphasis">$ 2,000,123</span>
+    </template>
+  </common-card>
 </template>
 
 <script>
 import commonCardMixin from '../../mixins/commonCardMixin'
 export default {
   name: 'TotalSales',
-  mixins: [commonCardMixin]
+  mixins: [commonCardMixin],
+  mounted () {
+    const chartDom = document.getElementById('total-orders-chart')
+    const chart = this.$echarts.init(chartDom)
+    chart.setOption({
+      xAxis: {
+        type: 'category',
+        show: false,
+        boundaryGap: false
+      },
+      yAxis: {
+        show: false
+      },
+      series: [{
+        type: 'line',
+        data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
+        areaStyle: {
+          color: 'purple'
+        },
+        lineStyle: {
+          width: 0
+        },
+        itemStyle: {
+          opacity: 0
+        },
+        smooth: true
+      }],
+      grid: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+      }
+    })
+  }
 }
 </script>
-
-<style>
-
-</style>

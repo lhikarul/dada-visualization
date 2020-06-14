6261caf1d926654157544259dca674bb63bfaf70
lhiakrul <711hikaru@gmail.com>
2020-06-14 21:01:31 +0800

    add: 銷售趨勢組件 開發 下

--- data-report-dev/src/components/SalesView/index.vue
+++ data-report-dev/src/components/SalesView/index.vue
@@ -3,7 +3,11 @@
     <el-card shadow="hover">
       <template v-slot:header>
         <div class="menu-wrapper">
-          <el-menu mode="horizontal" :default-active="activeIndex" @select="onMenuSelect">
+          <el-menu mode="horizontal"
+            :default-active="activeIndex"
+            @select="onMenuSelect"
+            class="sales-view-el-menu"
+            >
             <el-menu-item index="1">銷售額</el-menu-item>
             <el-menu-item index="2">訪問量</el-menu-item>
           </el-menu>
@@ -21,7 +25,9 @@
               start-placeholder="開始日期"
               end-placeholder="結束日期"
               size="small"
+              unlink-panels
               :picker-options="pickerOptions"
+              class="sales-view-date-picker"
               ></el-date-picker>
           </div>
         </div>
@@ -43,19 +49,28 @@ export default {
           {
             text: '最近一周',
             onClick (picker) {
-
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+              picker.$emit('pick', [start, end])
             }
           },
           {
             text: '最近一個月',
             onClick (picker) {
-
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 30)
+              picker.$emit('pick', [start, end])
             }
           },
           {
             text: '最近三個月',
             onClick (picker) {
-
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
+              picker.$emit('pick', [start, end])
             }
           }
         ]
@@ -75,6 +90,28 @@ export default {
   margin-top: 20px;
   .menu-wrapper {
     display: flex;
+    position: relative;
+    .sales-view-el-menu {
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+    .menu-right {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      .sales-view-date-picker {
+        margin-left: 20px;
+      }
+    }
   }
 }
 </style>
